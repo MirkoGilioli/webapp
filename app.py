0e6e5807ef8765e4d1f1d2dd31f4dfae787bb6ca
MirkoGilioli <50610475+MirkoGilioli@users.noreply.github.com>
2022-01-25 15:47:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/hello")
 def hello_world():
     return "Hello datatonic!"
-
+#This method is a dev a methos
 @app.route("/dev")
 def dev():
     return "This is the dev page"
